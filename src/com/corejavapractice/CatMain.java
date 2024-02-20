ead5b7a03ef80b2da8830ceb19187617544298d1
Manoj2068 <mmahato.student@wust.edu>
2024-02-20 12:08:01 -0500

    remove code

--- src/com/corejavapractice/CatMain.java
+++ src/com/corejavapractice/CatMain.java
@@ -3,7 +3,7 @@ package com.corejavapractice;
 public class CatMain {
 public static void main(String[] args) {
 	System.out.println("hello java");
-	System.out.println("hello world");
+ 
 	System.out.println("third commit");
 }
 }
